db8cb6bdee0b24efa191694017b1ec2903e21042
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-03-20 08:44:15 +0300

    Challenge Done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-//student grade generator
+/*
+Challenge 1: Student Grade Generator.
+Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+*/
+
+//Challenge 1:
+//Function that takes in an input between 0-100 and returns the output of the correct grade
 // const grade= number;
+//else the output is an error.
 function gradeGenerator(number){
     if(number>79){
         return 'A';
@@ -17,7 +25,12 @@ function gradeGenerator(number){
 }
 gradeGenerator();
 //end of program
-//speed detector
+
+/*Challenge 2: Speed Detector.
+Write a program that takes as input the speed of a car e.g 80. 
+If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+*/
 //const speed=num;
 const speedDetector=function(num){
     if (num<=70){
